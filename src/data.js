51ef98d47655b4649b2e2beeea057f98a8e536c7
razieh boroon <razieh.boroon@gmail.com>
2022-07-24 19:32:04 +0430

    data updated

--- src/data.js
+++ src/data.js
@@ -11,9 +11,9 @@ export const data = [
   {
     id: 2,
     image: "/assets/images/Store.png",
-    name: "Store",
+    name: "Online Store",
     description:
-      "A more professional version of my Plant Store App by taking advatage of SASS, Reducer, Context, React Router Dom, Prop-Types, Paypal Sandbox, axios and Fakesstore.com API. By the way, this app also has mobile first design.",
+      "A responsive online store developed by React.js and by taking advatage of SASS, Reducer, Context, React Router Dom V5.2, Prop-Types, Paypal Sandbox, axios and Fakesstore.com API. By the way, this app also has mobile first design.",
     git: "https://github.com/raziehboroon/store-project.git",
     netlify: "https://rboroon-store.netlify.app/",
   },
@@ -22,17 +22,17 @@ export const data = [
     image: "/assets/images/MovieDB.png",
     name: "MovieDB App",
     description:
-      "A movie Database app developed by React.js and taking advatage of SASS, Context, React Router Dom, Material-ui, React Alice Carousel and Themoviedb.org API.",
+      "A responsive Movie Database developed by React.js and taking advatage of SASS, Context, React Router Dom V6, Material-ui, React Alice Carousel, axios and Themoviedb.org API.",
     git: "https://github.com/raziehboroon/react-moviedb",
     netlify: "https://rboroon-moviedb-app.netlify.app/",
   },
-  {
-    id: 4,
-    image: "/assets/images/plantstore.png",
-    name: "Plant Store",
-    description:
-      "A online shop app developed by React.js and taking advatage of Bootstrap, SASS, React Router Dom, Prop-Types and static data. By the way, this app has mobile first design.",
-    git: "https://github.com/raziehboroon/react-plant-store",
-    netlify: "https://rboroon-plant-store.netlify.app",
-  },
+  // {
+  //   id: 4,
+  //   image: "/assets/images/plantstore.png",
+  //   name: "Plant Store",
+  //   description:
+  //     "A online shop app developed by React.js and taking advatage of Bootstrap, SASS, React Router Dom, Prop-Types and static data. By the way, this app has mobile first design.",
+  //   git: "https://github.com/raziehboroon/react-plant-store",
+  //   netlify: "https://rboroon-plant-store.netlify.app",
+  // },
 ];
